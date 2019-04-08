ddb6c4e3169e26333e705e5e65ac6034e6544000
kouhei ueno <k.cola119@gmail.com>
2019-04-08 15:59:47 +0900

    create response messages

--- app/controllers/api/v1/lc_classes_controller.rb
+++ app/controllers/api/v1/lc_classes_controller.rb
@@ -2,29 +2,34 @@ class Api::V1::LcClassesController < ApplicationController
 	before_action :require_have_comp
 
 	def index
-		@classes = @comp.lc_classes.all
-		render json: @classes
+		@classes = @comp.lc_classes.order(created_at: :desc)
+		return response_not_found(:classes) if @classes.blank?
+		response_success(:classes, :index, @classes)
 	end
 
 	def show
 		@class = @comp.lc_classes.find_by_id(params[:id])
-		render json: @class
+		return response_not_found(:class) if @class.nil?
+		response_success(:class, :show, @class)
 	end
 
 	def create
 		@class = @comp.lc_classes.build(class_params)
-		render json: @class.save ? @class : @class.errors
+		return response_bad_request unless @class.valid?
+		@class.save ? response_success(:class, :create, @class) : response_internal_server_error
 	end
 
 	def destroy
 		@class = LcClass.find_by_id(params[:id])
-		@class.destroy unless @class.nil?
-		render json: @class
+		return response_not_found(:class) if @class.nil?
+		@class.destroy
+		response_success(:class, :destroy, @class)
 	end
 
 	def update
 		@class = LcClass.find_by_id(params[:id])
-		render json: @class.nil? ? nil : @class.update(class_params) ? @class : @class.errors
+		return response_not_found(:class) if @class.nil?
+		@class.update(class_params) ? response_success(:class, :update, LcClass.find_by_id(params[:id])) : response_internal_server_error
 	end
 
 	private
@@ -34,9 +39,6 @@ class Api::V1::LcClassesController < ApplicationController
 
 		def require_have_comp
 			@comp = LcComp.find_by_id(params[:comp_id])
-			if @comp.nil?
-				render json: {"status": "error"}
-				return
-			end
+			return response_bad_request if @comp.nil?
 		end
 end
